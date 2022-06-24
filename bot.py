488a1e889f3941e339dcdb3dabd46fb6cc0a8e4f
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-06-24 07:54:19 -0700

    Using Tweepy instead. Getting 401 error

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import gspread
+import config
+import tweepy
 
 gc = gspread.service_account('credentials.json')
 
@@ -15,6 +17,26 @@ with open('facts.txt') as my_file:
     for line in my_file:
         testsite_array.append(line)
 
-for i in range(1, len(testsite_array)+1):
-    wks.update_cell(i, 1, testsite_array[i-1])
+# for i in range(1, len(testsite_array)+1):
+#     wks.update_cell(i, 1, testsite_array[i-1])
 
+# t = Twitter(
+#     auth=OAuth2(bearer_token = config.bearer_token))
+    
+# print(config.token)
+
+# t.updateStatus("Testing!")
+
+#TWEEPY
+
+client = tweepy.Client(
+    # bearer_token=config.bearer_token, 
+    consumer_key=config.consumer_key, 
+    consumer_secret=config.consumer_secret, 
+    access_token=config.acces_token,
+    access_token_secret= config.acces_token_secrets,
+    # return_type=Response, wait_on_rate_limit=False
+    )
+    
+response = client.create_tweet(text = "Test!")
+print(response)
