b7df31450bff6129a945c33ba816abeaba61ab18
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-06-28 06:17:58 -0700

    Posted a tweet!

--- bot.py
+++ bot.py
@@ -1,42 +1,15 @@
-import gspread
-import config
 import tweepy
+import config
 
-gc = gspread.service_account('credentials.json')
-
-# Open a sheet from a spreadsheet in one go
-wks = gc.open("google api").sheet1
-
-# python-api@api-shtuff.iam.gserviceaccount.com
-# for x in range(10):
-#     wks.update('A', [1, 2])
-# wks.update('A1', [[1, 2], [3,4]])
-
-testsite_array = []
-with open('facts.txt') as my_file:
-    for line in my_file:
-        testsite_array.append(line)
-
-# for i in range(1, len(testsite_array)+1):
-#     wks.update_cell(i, 1, testsite_array[i-1])
-
-# t = Twitter(
-#     auth=OAuth2(bearer_token = config.bearer_token))
-    
-# print(config.token)
-
-# t.updateStatus("Testing!")
-
-#TWEEPY
-
+# calling a client
 client = tweepy.Client(
-    # bearer_token=config.bearer_token, 
-    consumer_key=config.consumer_key, 
-    consumer_secret=config.consumer_secret, 
-    access_token=config.acces_token,
-    access_token_secret= config.acces_token_secrets,
-    # return_type=Response, wait_on_rate_limit=False
-    )
-    
-response = client.create_tweet(text = "Test!")
+    consumer_key=config.consumer_key,
+    consumer_secret=config.consumer_secret,
+    access_token=config.access_token,
+    access_token_secret=config.access_token_secret
+)
+
+response = client.create_tweet(text='twitter api testoooor')
+
+
 print(response)
